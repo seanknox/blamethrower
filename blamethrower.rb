7dcb21df897efeefaef9973a377382abfb9c01ed
Sean Knox <knoxville@gmail.com>
2013-01-07 14:55:38 -0800

    blamethrower is alive and full of fire.

---
+++ blamethrower.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby 
+
+files = `git ls-tree -r HEAD #{ARGV.join(" ")}| cut -f 2`
+
+counthash = Hash.new {0}
+
+files.lines.each do |file|
+  puts "FILE: #{file}"
+  filecount = `git blame --line-porcelain #{file.chomp} | grep \"author \" |sort|uniq -c |sort -nr`.lines
+
+  filecount.each do |line|
+    begin 
+    matchdata = line.match /(\d+)\s+author\s+(.*)/ 
+    author = matchdata[2]  
+    linecount = matchdata[1].to_i
+    counthash[author] += linecount 
+    rescue 
+    end
+  end
+
+end
+
+sorted_array = counthash.sort_by {|k,v| -v.to_i}
+
+sorted_array.each {|author,lines| puts "#{author}: #{lines}" }
+
